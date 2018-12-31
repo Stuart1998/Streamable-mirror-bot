6adf884392d450e6b4c4b251c630ba9ba9fda770
O-Gourari <34598597+O-Gourari@users.noreply.github.com>
2018-12-31 11:07:16 +0100

    Update const.py

--- const.py
+++ const.py
@@ -3,11 +3,8 @@
 UPLOAD = 'https://api.streamable.com/import?url={url}'
 VIDEO_URL = 'https://streamable.com/{shortcode}'
 HEADERS = {'User-Agent': 'Streamable mirroring reddit bot'}
-REPLY_MESSAGE = ('Streamable mirror: {link}\n\n'
-				 '*I am a bot, and this action was performed automatically. '
-				 'Please [contact the moderators of this subreddit]'
-				 '(https://www.reddit.com/message/compose?to=%2Fr%2F{sub_name})'
-				 ' if you have any questions or concerns.*')
+REPLY_MESSAGE = ('[Streamable Mirror]({link})\n\n'
+				 '*I am a bot, and this action was performed automatically.*')
 TIMEOUT = 5
 LOG_MSG = 'submission: {} mirror: {}'
 LOG_FILE = 'logs.log'
